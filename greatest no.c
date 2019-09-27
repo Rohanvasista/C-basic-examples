0f98fb30c7848ff7a7d526c0143ee62e0ff08e7a
Rohanvasista <53577853+Rohanvasista@users.noreply.github.com>
2019-09-28 00:37:09 +0530

    Create greatest no.c

---
+++ greatest no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a=8,b=15,num;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(num<10)
+    {
+        printf("a is greater");
+    }
+    else
+    {
+        printf("b is greater");
+        
+    }
+    return 0;
+}
